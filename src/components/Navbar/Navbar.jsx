ee9d0a774d3a4c8d53523131ffbcb64a0f4c26ac
Ebrahim574 <eghonim98@gmail.com>
2025-07-10 20:59:54 +0300

    5th commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import logo from "../../assets/imgi_2_logo-sm.png";
 export default function Navbar({ onLinkClick }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleClick = (name) => {
     onLinkClick(name);
   };
@@ -16,7 +18,29 @@ export default function Navbar({ onLinkClick }) {
               Game Reviews
             </span>
           </a>
+          <button
+            type="button"
+            onClick={() => setIsOpen(!isOpen)}
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
 
+          {/* 
           <button
             data-collapse-toggle="navbar-default"
             type="button"
@@ -40,12 +64,9 @@ export default function Navbar({ onLinkClick }) {
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
-          </button>
+          </button> */}
 
-          <div
-            className="hidden w-full md:block md:w-auto"
-            id="navbar-default"
-          >
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col py-4 md:p-0 mt-4 border rounded-3xl md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li onClick={() => handleClick("mmorpg")}>
                 <a
