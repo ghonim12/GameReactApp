b79b993298db00a55df4065ea75f72737c920748
Ebrahim574 <eghonim98@gmail.com>
2025-07-10 21:14:06 +0300

    8th commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,9 +22,9 @@ export default function Navbar({ onLinkClick }) {
           <button
             type="button"
             onClick={() => setIsOpen(!isOpen)}
-            data-collapse-toggle="navbar-default"
-            aria-controls="navbar-default"
-            aria-expanded="false"
+            // data-collapse-toggle="navbar-default"
+            // aria-controls="navbar-default"
+            // aria-expanded="false"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           >
             <span className="sr-only">Open main menu</span>
@@ -70,7 +70,7 @@ export default function Navbar({ onLinkClick }) {
             </svg>
           </button> */}
 
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className={`${isOpen ? 'block' : 'hidden'} md:block w-full md:w-auto`} id="navbar-default">
             <ul className="font-medium flex flex-col py-4 md:p-0 mt-4 border rounded-3xl md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li onClick={() => handleClick("mmorpg")}>
                 <a
